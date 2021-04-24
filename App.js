4b1d95eda1f5c6c88282973b1a5d31698f6a916d
shankaryadav <shankaryadav0130@gmail.com>
2021-04-24 22:53:22 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import screens
+import HomeScreen from "./screens/HomeScreen";
+import AddNewContactScreen from "./screens/AddNewContactScreen";
+import EditContactScreen from "./screens/EditContactScreen";
+import ViewContactScreen from "./screens/ViewContactScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+//import react navigation
+import {  createAppContainer } from "react-navigation";
+import { createStackNavigator } from 'react-navigation-stack';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const MainNavigator = createStackNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Add: { screen: AddNewContactScreen },
+    View: { screen: ViewContactScreen },
+    Edit: { screen: EditContactScreen }
   },
-});
+  {
+    defaultNavigationOptions: {
+      headerTintColor: "#fff",
+      headerStyle: {
+        backgroundColor: "#b83227"
+      },
+      headerTitleStyle: {
+        color: "#fff"
+      }
+    }
+  }
+);
+
+const App = createAppContainer(MainNavigator);
+export default App;
